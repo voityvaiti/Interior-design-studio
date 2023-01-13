7a7b946ba75bf7da2a687ada37706d50f9706930
voityvaiti <flashlight@i.ua>
2023-01-13 21:12:18 +0200

    Validator replaced by Hibernate Validator

--- src/main/java/com/myproject/idstudio/models/Call.java
+++ src/main/java/com/myproject/idstudio/models/Call.java
@@ -1,8 +1,18 @@
 package com.myproject.idstudio.models;
 
+
+
+import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
 public class Call {
 	private int id;
+	@NotBlank(message = "Please, enter your name")
+	@Size(max = 40, message = "Too long name!")
 	private String name;
+	@NotBlank(message = "Please, enter your telephone number")
+	@Size(max = 16, message = "Too long telephone number!")
 	private String number;
 	
 	public Call(String name, String number) {
--- src/main/java/com/myproject/idstudio/servlets/CallOrderServlet.java
+++ src/main/java/com/myproject/idstudio/servlets/CallOrderServlet.java
@@ -3,6 +3,7 @@ package com.myproject.idstudio.servlets;
 import java.io.IOException;
 
 import java.sql.SQLException;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,41 +11,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;;
 import com.myproject.idstudio.dao.CallDao;
 import com.myproject.idstudio.dao.CustomerDao;
 import com.myproject.idstudio.models.Call;
 import com.myproject.idstudio.models.Customer;
-import com.myproject.idstudio.validators.CallOrderValidator;
 
 @WebServlet("/order")
 public class CallOrderServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/orderCall.jsp").forward(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/view/orderCall.jsp").forward(request, response);
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		String name = request.getParameter("name");
-		String number = request.getParameter("number");
-		session.setAttribute("name", name);
-		session.setAttribute("number", number);
-		
-		CallOrderValidator callOrderValidator = new CallOrderValidator(new Call(name, number));
-		if(callOrderValidator.hasErrors()) {
-			session.setAttribute("callOrderErrorMessage", callOrderValidator.getErrors());
-			doGet(request, response);
-			session.removeAttribute("callOrderErrorMessage");
-		}
-		else {
-		try {
-			CallDao.getInstance().addCall(new Call(name, number));
-			CustomerDao.getInstance().addCustomer(new Customer(name, "", number));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		response.sendRedirect(request.getContextPath() + "/");
-		}
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String name = request.getParameter("name");
+        String number = request.getParameter("number");
+        session.setAttribute("name", name);
+        session.setAttribute("number", number);
 
+        Set<ConstraintViolation<Call>> errors = Validation.buildDefaultValidatorFactory().
+                getValidator().validate(new Call(name, number));
+        if(errors.isEmpty()) {
+            try {
+                CallDao.getInstance().addCall(new Call(name, number));
+                CustomerDao.getInstance().addCustomer(new Customer(name, "", number));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            response.sendRedirect(request.getContextPath() + "/");
+        }
+        else {
+            String callOrderErrorMessage = String.join("<br>",
+                    errors.stream().map(error -> error.getMessage()).toList());
+            session.setAttribute("callOrderErrorMessage", callOrderErrorMessage);
+            doGet(request, response);
+            session.removeAttribute("callOrderErrorMessage");
+        }
+    }
 }
